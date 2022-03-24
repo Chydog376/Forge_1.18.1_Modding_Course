05f071c0f2199b7954b2707abac513515adf1233
Chydog376 <hankse6@gmail.com>
2022-03-23 21:28:14 -0400

    Added Cobalt Block! (No name and texture)

--- src/main/java/com/chydog/mcc/MccMod.java
+++ src/main/java/com/chydog/mcc/MccMod.java
@@ -1,5 +1,6 @@
 package com.chydog.mcc;
 
+import com.chydog.mcc.block.ModBlocks;
 import com.chydog.mcc.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -32,6 +33,7 @@ public class MccMod
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+        ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
 
